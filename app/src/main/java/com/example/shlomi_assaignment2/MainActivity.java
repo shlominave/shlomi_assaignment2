6423c2f465909e02ee76bf6d35e3043822737bfe
shlominave <s1170@motzkine.ort.org.il>
2020-10-19 14:59:06 +0300

    assaignment 2

--- app/src/main/java/com/example/shlomi_assaignment2/MainActivity.java
+++ app/src/main/java/com/example/shlomi_assaignment2/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.shlomi_assaignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+int cnt=0;
+TextView TV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TV=findViewById(R.id.txtResult);
+
+    }
+
+    public void OnClick(View view) {
+        cnt++;
+        String str=getString(R.string.textOnTextBox);
+        TV.setText(str+ " "+ cnt);
     }
 }
